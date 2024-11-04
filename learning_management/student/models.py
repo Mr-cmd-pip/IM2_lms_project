ab8da8ed711f7d6adfc3a6a4e5b43dcb75837194
Eman <orientaleac@gmail.com>
2024-11-04 20:37:25 +0800

    Cagampang-First-commit

--- learning_management/student/models.py
+++ learning_management/student/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
